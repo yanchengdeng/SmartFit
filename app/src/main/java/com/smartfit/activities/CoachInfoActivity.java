54bffcb1715e68ed04cf5812818871f4fa0aaaa6
yanchengdeng <yanchengdeng@gmail.com>
2016-03-08 18:04:23 +0800

    增加列表加载功能  （上下拉加载）

--- app/src/main/java/com/smartfit/activities/CoachInfoActivity.java
+++ app/src/main/java/com/smartfit/activities/CoachInfoActivity.java
@@ -1,19 +1,105 @@
 package com.smartfit.activities;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.ecloud.pulltozoomview.PullToZoomScrollViewEx;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.smartfit.R;
+import com.smartfit.adpters.ChooseOrderAdapter;
+import com.smartfit.adpters.CoachAppraiseAdapter;
+import com.smartfit.views.MyListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 教练详情页
  */
 public class CoachInfoActivity extends BaseActivity {
-
+    private PullToZoomScrollViewEx scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach_info);
+        // 修改状态栏颜色，4.4+生效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus();
+        }
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.bar_regiter_bg);//通知栏所需颜色
+        loadViewForCode();
+        scrollView = (PullToZoomScrollViewEx) findViewById(R.id.scroll_view);
+        initView();
+        addLisener();
+        DisplayMetrics localDisplayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(localDisplayMetrics);
+        int mScreenHeight = localDisplayMetrics.heightPixels;
+        int mScreenWidth = localDisplayMetrics.widthPixels;
+        LinearLayout.LayoutParams localObject = new LinearLayout.LayoutParams(mScreenWidth, (int) (9.0F * (mScreenWidth / 16.0F)));
+        scrollView.setHeaderLayoutParams(localObject);
     }
 
 
+
+    private void loadViewForCode() {
+        PullToZoomScrollViewEx scrollView = (PullToZoomScrollViewEx) findViewById(R.id.scroll_view);
+        View headView = LayoutInflater.from(this).inflate(R.layout.coach_header_ui, null, false);
+        View zoomView = LayoutInflater.from(this).inflate(R.layout.coach_hade_zoom_view, null, false);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.coach_content_ui, null, false);
+        scrollView.setHeaderView(headView);
+        scrollView.setZoomView(zoomView);
+        scrollView.setScrollContentView(contentView);
+        scrollView.setParallax(true);
+
+    }
+
+    MyListView myListView;
+    RatingBar ratingBar;
+    View tvMoreAppraise,tvCoachMoreInfo;
+
+    private void initView() {
+        myListView = (MyListView) scrollView.getPullRootView().findViewById(R.id.listView);
+        List<String> appraises = new ArrayList<>();
+        appraises.add("");
+        myListView.setAdapter(new CoachAppraiseAdapter(this, appraises));
+        tvMoreAppraise = scrollView.getPullRootView().findViewById(R.id.tv_read_more_discuss);
+        tvCoachMoreInfo = scrollView.getPullRootView().findViewById(R.id.tv_read_more_info);
+        ratingBar = (RatingBar) scrollView.getPullRootView().findViewById(R.id.ratingBar);
+        ratingBar.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 24));
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.scrollTo(0, 0);
+            }
+        });
+
+    }
+
+
+    private void addLisener(){
+        tvMoreAppraise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(CoachAppraiseActivity.class);
+            }
+        });
+
+        tvCoachMoreInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(CoachAuthentitionActivity.class);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/smartfit/adpters/CoachAppraiseAdapter.java
@@ -0,0 +1,92 @@
+package com.smartfit.adpters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.smartfit.R;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * 教练评价列表  item
+ * Created by Administrator on 2016/3/8.
+ */
+public class CoachAppraiseAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> datas;
+    private List<String> data;
+    RelativeLayout.LayoutParams params;
+
+    public CoachAppraiseAdapter(Context context, List<String> datas) {
+        this.context = context;
+        this.datas = datas;
+        params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,24);
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT ,RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.CENTER_VERTICAL);
+    }
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return datas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewhold;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.adapter_coach_appraise_item, null);
+            viewhold = new ViewHolder(convertView);
+            convertView.setTag(viewhold);
+        } else {
+            viewhold = (ViewHolder) convertView.getTag();
+        }
+
+        viewhold.ratingBar.setLayoutParams(params);
+        return convertView;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * This class contains all butterknife-injected Views & Layouts from layout file 'adapter_coach_appraise_item.xml'
+     * for easy to all layout elements.
+     *
+     * @author ButterKnifeZelezny, plugin for Android Studio by Avast Developers (http://github.com/avast)
+     */
+    static class ViewHolder {
+        @Bind(R.id.tv_date)
+        TextView tvDate;
+        @Bind(R.id.tv_join)
+        TextView tvJoin;
+        @Bind(R.id.ratingBar)
+        RatingBar ratingBar;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
